9040c6d879d23ea73a8bc9cb250a7f95a91ffbf8
Roizus <alvaroroizo@gmail.com>
2016-01-23 14:03:15 +0100

    Modify Layout SignUpActiviy and add intent

--- app/src/main/java/es/dev_burnchat/burnchat/LoginActivity.java
+++ app/src/main/java/es/dev_burnchat/burnchat/LoginActivity.java
@@ -1,13 +1,26 @@
 package es.dev_burnchat.burnchat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView mSignupTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mSignupTextView=(TextView)findViewById(R.id.SignupButton);
+        mSignupTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
