06010e005ec9b401c36905120491a09d1d2f8479
Roizus <alvaroroizo@gmail.com>
2016-01-28 20:26:25 +0100

    Add list friends in FriendsFragment

--- app/src/main/java/es/dev_burnchat/burnchat/FriendsFragment.java
+++ app/src/main/java/es/dev_burnchat/burnchat/FriendsFragment.java
@@ -1,17 +1,35 @@
 package es.dev_burnchat.burnchat;
 
-
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 /**
  * Created by Alvaro on 26/1/16.
  */
 public class FriendsFragment extends ListFragment{
+
+    public static final String TAG = FriendsFragment.class.getSimpleName();
+
+    ParseRelation<ParseUser> mFriendsRelation;
+    ParseUser mCurrentUser;
+    List<ParseUser> mFriends;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
@@ -19,6 +37,50 @@ public class FriendsFragment extends ListFragment{
         ProgressBar spinner=(ProgressBar) rootView.findViewById(R.id.progressBar);
         spinner.setVisibility(View.GONE);
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.FRIENDS_RELATION);
+
+        getActivity().setProgressBarIndeterminateVisibility(true);
+
+        ParseQuery<ParseUser> query = mFriendsRelation.getQuery();
+        query.addAscendingOrder(ParseConstants.USERNAME);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> friends, ParseException e) {
+                getActivity().setProgressBarIndeterminateVisibility(false);
+
+                if (e == null) {
+                    mFriends = friends;
+
+                    String[] usernames = new String[mFriends.size()];
+                    int i = 0;
+                    for(ParseUser user : mFriends) {
+                        usernames[i] = user.getUsername();
+                        i++;
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                            getListView().getContext(),
+                            android.R.layout.simple_list_item_1,
+                            usernames);
+                    setListAdapter(adapter);
+                }
+                else {
+                    Log.e(TAG, e.getMessage());
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getListView().getContext());
+                    builder.setMessage(e.getMessage())
+                            .setTitle(R.string.error_message)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
     }
     }
 
