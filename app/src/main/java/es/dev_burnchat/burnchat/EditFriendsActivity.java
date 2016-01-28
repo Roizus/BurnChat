d1ca342537492679ee47f5946a4b617b53fe81ef
Roizus <alvaroroizo@gmail.com>
2016-01-28 20:17:19 +0100

    Add changes checkbox listview

--- app/src/main/java/es/dev_burnchat/burnchat/EditFriendsActivity.java
+++ app/src/main/java/es/dev_burnchat/burnchat/EditFriendsActivity.java
@@ -30,7 +30,7 @@ public class EditFriendsActivity extends ListActivity {
     ArrayList<String> usernames;
     ArrayAdapter<String> adapter;
     ProgressBar spinner;
-    ParseRelation<ParseUser> mFriendsRelation;
+    private ParseRelation<ParseUser> mFriendsRelation;
     private ParseUser mCurrentUser;
 
     @Override
@@ -42,16 +42,11 @@ public class EditFriendsActivity extends ListActivity {
 
     }
 
-    @Override
+  /* @Override
     protected void onResume() {
         super.onResume();
         mCurrentUser=ParseUser.getCurrentUser();
         mFriendsRelation=mCurrentUser.getRelation(ParseConstants.FRIENDS_RELATION);
-
-        //spinner.setVisibility(View.VISIBLE);
-        usernames=new ArrayList<String>();
-        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_checked,usernames);
-        setListAdapter(adapter);
 
         ParseQuery query= ParseUser.getQuery();
         query.orderByAscending(ParseConstants.USERNAME);
@@ -62,9 +57,19 @@ public class EditFriendsActivity extends ListActivity {
                 if(e==null){
                 //sucess
                     mUsers=users;
+                    String[] usernames = new String[mUsers.size()];
+                    int i = 0;
                     for(ParseUser user:mUsers){
-                        adapter.add(user.getUsername());
+                        //adapter.add(user.getUsername());
+                        usernames[i] = user.getUsername();
                     }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                            EditFriendsActivity.this,
+                            android.R.layout.simple_list_item_checked,
+                            usernames);
+                    setListAdapter(adapter);
+
+                    addFriendCheckmarks();
                 }
                 else{
                     Log.e(TAG,"ParseExceptioncaught:",e);
@@ -74,8 +79,55 @@ public class EditFriendsActivity extends ListActivity {
             }
         });
 
+    }*/
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.FRIENDS_RELATION);
+
+        setProgressBarIndeterminateVisibility(true);
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.orderByAscending(ParseConstants.USERNAME);
+        query.setLimit(1000);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+                setProgressBarIndeterminateVisibility(false);
+
+                if (e == null) {
+                    // Success
+                    mUsers = users;
+                    String[] usernames = new String[mUsers.size()];
+                    int i = 0;
+                    for(ParseUser user : mUsers) {
+                        usernames[i] = user.getUsername();
+                        i++;
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                            EditFriendsActivity.this,
+                            android.R.layout.simple_list_item_checked,
+                            usernames);
+                    setListAdapter(adapter);
+
+                    addFriendCheckmarks();
+                }
+                else {
+                    Log.e(TAG, e.getMessage());
+                    AlertDialog.Builder builder = new AlertDialog.Builder(EditFriendsActivity.this);
+                    builder.setMessage(e.getMessage())
+                            .setTitle(R.string.error_message)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
     }
+
 
 
     @Override
@@ -104,6 +156,31 @@ public class EditFriendsActivity extends ListActivity {
             @Override
             public void done(ParseException e) {
                 if (e != null) {
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        });
+    }
+
+    private void addFriendCheckmarks() {
+        mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> friends, ParseException e) {
+                if (e == null) {
+                    // list returned - look for a match
+                    for (int i = 0; i < mUsers.size(); i++) {
+                        ParseUser user = mUsers.get(i);
+
+                        for (ParseUser friend : friends) {
+                            if (friend.getObjectId().equals(user.getObjectId())) {
+                                getListView().setItemChecked(i, true);
+                            }
+                        }
+                    }
+                    spinner.setVisibility(View.INVISIBLE);
+
+                }
+                else {
                     Log.e(TAG, e.getMessage());
                 }
             }
